1f37312470c75c942ecfc0b4221d166cea115b26
Olivier Bossel <olivier.bossel@gmail.com>
2018-12-18 13:28:15 +0100

    fetch todos from fake rest api

--- src/containers/TodoPage/sagas.js
+++ src/containers/TodoPage/sagas.js
@@ -1,14 +1,12 @@
 import { put, takeEvery } from 'redux-saga/effects'
+import axios from 'axios'
 import { FETCH_TODOS, TODOS_FETCHED } from './constants'
 
 function* fetchTodos(action) {
-  const todos = [
-    { id: 1, text: 'Hello World', done: false },
-    { id: 2, text: 'Hello Universe', done: true },
-    { id: 3, text: 'Hello Jupiter', done: false },
-    { id: 4, text: 'Hello the Moon', done: true }
-  ]
-  yield put({ type: TODOS_FETCHED, todos })
+  const response = yield axios.get(
+    'https://my-json-server.typicode.com/coffeekraken/react-native-boilerplate/todos'
+  )
+  yield put({ type: TODOS_FETCHED, todos: response.data })
 }
 
 export default function* mySaga() {
